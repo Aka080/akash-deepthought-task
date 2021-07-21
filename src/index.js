a93b763403e160d9bd631deb74057c137867cd06
Akash Singh <Aka080@users.noreply.github.com>
2021-07-22 04:27:04 +0530

    installed router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <Router>
+     <Switch>
+       <Route path='/'>
+       <App />
+       </Route>
+       <Route path='*'>
+       <App />
+       </Route>
+  
+     </Switch>
+     
+     </Router> 
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +33,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
